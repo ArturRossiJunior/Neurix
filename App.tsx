dba4f152765871663ebc1bca029c2db21117db34
ARTUR ROSSI JUNIOR <082210044@faculdade.cefsa.edu.br>
2025-11-06 20:32:40 -0300

    Teste apk

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <AppNavigator />
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <AppNavigator />
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
