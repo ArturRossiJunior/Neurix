484e84b2048900707891279c311f3d46f2dcff8a
ARTUR ROSSI JUNIOR <082210044@faculdade.cefsa.edu.br>
2025-11-06 20:50:58 -0300

    Ajuste pequeno no README [skip ci]

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <AuthProvider>
-        <AppNavigator />
-      </AuthProvider>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
   );
 }
