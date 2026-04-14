0828bac8609f91b479a68d0aff773a6c489a3e43
Gustavo Carvalho <gustavo.correa2025@gmail.com>
2026-04-14 11:22:25 -0300

    Novo teste

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dmwiewbouomfzovlbcme.supabase.co';
-const supabaseAnonKey = 'sb_publishable_bEhtxCWVLsWosp30Zc9jzg_lbSsv9xm';
+//const supabaseUrl = 'https://dmwiewbouomfzovlbcme.supabase.co';
+//const supabaseAnonKey = 'sb_publishable_bEhtxCWVLsWosp30Zc9jzg_lbSsv9xm';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
